a78f107fc15cbaca50baf8094fffaf13d4efc342
chriscampaigne <campaignemc@gmail.com>
2022-10-19 19:27:20 -0400

    added css loaders and babel to webpack

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -28,3 +28,26 @@ module.exports = () => {
     },
   };
 };
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  rules: [
+    {
+      test: /\.m?js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    }
+  ]
+};
