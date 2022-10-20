0744a12c5b996ec2a03f5a9d2bfe669ad4e78b0b
chriscampaigne <campaignemc@gmail.com>
2022-10-19 20:45:22 -0400

    added logic for put method

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,7 +13,18 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (id,value) => {
+  console.error('Request to update jateDB');
+  const contactDb = await openDB('jate', 1);
+
+  const tx = contactDb.transaction('jate', 'readwrite');
+
+  const store = tx.objectStore('jate');
+  
+  const request = store.put({ id: id, value: value});
+  const result = await request;
+  console.log(' data saved to the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async (value) => {
@@ -22,7 +33,8 @@ const contactDb = await openDB('jate', 1);
 const tx = contactDb.transaction('jate', 'readwrite');
 const store = tx.objectStore('jate');
 const request = store.getAll();
-const res = await req;
+const result = await request;
+console.log('data saved,', result)
 }
 
 
