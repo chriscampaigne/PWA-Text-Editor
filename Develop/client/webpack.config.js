ea5269564168f126ca33054abad3a86d27eabb2e
chriscampaigne <campaignemc@gmail.com>
2022-10-19 22:11:02 -0400

    added event listeners to install app and manifest add on

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -22,6 +22,26 @@ module.exports = () => {
         template: './index.html',
         title: 'JATE'
       }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      new WebpackPwaManifest({
+        name: 'PWA Text Editor',
+        short_name: 'Text Editor',
+        description: 'Edit Text!',
+        background_color: '#7eb4e2',
+        theme_color: '#7eb4e2',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+       ],
+      }),
     ],
 
     module: {
